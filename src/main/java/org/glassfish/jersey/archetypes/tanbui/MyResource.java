7682e9985517af706aba6c93069a9de70cbeb178
Tan Bui at home <tan.bui.home@gmail.com>
2017-04-27 10:52:17 +0300

    update ignore after rm cached

---
+++ src/main/java/org/glassfish/jersey/archetypes/tanbui/MyResource.java
@@ -0,0 +1,14 @@
+package org.glassfish.jersey.archetypes.tanbui;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
+
+public class MyResource extends ResourceConfig {
+
+    public MyResource(){
+        register(RequestContextFilter.class);
+        register(JerseyResource.class);
+        register(SpringSingletonResource.class);
+    }
+
+}
