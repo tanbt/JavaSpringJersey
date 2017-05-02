1c4aed9b75250b585cb262188bd36501b096db1d
Tan Bui at home <tan.bui.home@gmail.com>
2017-05-02 22:55:17 +0300

    findAll with paging

--- src/main/java/org/glassfish/jersey/archetypes/tanbui/service/GroupService.java
+++ src/main/java/org/glassfish/jersey/archetypes/tanbui/service/GroupService.java
@@ -3,15 +3,15 @@ package org.glassfish.jersey.archetypes.tanbui.service;
 import org.glassfish.jersey.archetypes.tanbui.entity.Group;
 import org.glassfish.jersey.archetypes.tanbui.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
-
 
 public class GroupService {
     @Autowired
     private GroupRepository groupRepository;
 
     public List<Group> getGroups(int page, int size) {
-        return groupRepository.findAll();
+        return groupRepository.findAll(new PageRequest(page, size)).getContent();
     }
 }
